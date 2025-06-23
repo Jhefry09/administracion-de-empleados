1ebcbbefc01e27888686ce82b2bf24bfd1c8917f
Jhefry09 <jhefrycabanillas@gmail.com>
2025-06-23 16:09:00 -0500

    creacion del metodo para mostrar empleados

--- main/java/modelo/ModelEmpleado.java
+++ main/java/modelo/ModelEmpleado.java
@@ -1,5 +1,35 @@
 package modelo;
 
-public class ModelEmpleado {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import entidad.Empleado;
+import utils.MySQLDBConexion;
+
+public class ModelEmpleado {
+public List<Empleado> mostrar(){
+	List<Empleado> datos = new ArrayList<Empleado>();
+	String sql = "SELECT * FROM empleados";
+	try {
+		Connection con = MySQLDBConexion.getConexion();
+		PreparedStatement pstm = con.prepareStatement(sql);
+		ResultSet rs = pstm.executeQuery();
+		while (rs.next()) {
+			Empleado emp = new Empleado();
+			emp.setId_emp(rs.getInt(1));
+			emp.setNom_emp(rs.getString(2));
+			emp.setApe_emp(rs.getString(3));
+			emp.setDni(rs.getString(5));
+			emp.setDepartamento(rs.getString(6));
+			emp.setPuesto(rs.getString(7));
+			emp.setSueldo(rs.getInt(8));
+			datos.add(emp);
+		}
+	} catch (Exception e) {
+		e.printStackTrace();
+	}return datos;
+}
 }
