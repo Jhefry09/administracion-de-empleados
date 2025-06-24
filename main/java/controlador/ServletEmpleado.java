2343d356b355f9361f2f80e819d0d686c14b0147
Jhefry09 <jhefrycabanillas@gmail.com>
2025-06-23 22:49:36 -0500

    creacion del servlet para mostrar los empleados

---
+++ main/java/controlador/ServletEmpleado.java
@@ -0,0 +1,40 @@
+package controlador;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entidad.Empleado;
+import modelo.ModelEmpleado;
+
+/**
+ * Servlet implementation class ServletEmpleado
+ */
+@WebServlet("/ServletEmpleado")
+public class ServletEmpleado extends HttpServlet {
+	ModelEmpleado memp = new ModelEmpleado();
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletEmpleado() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	List<Empleado> info = new ModelEmpleado().mostrar();
+	request.setAttribute("datos", info);
+	request.getRequestDispatcher("ListaEmpleados.jsp").forward(request, response);
+	}
+}
